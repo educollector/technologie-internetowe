27b430f3ce3919b925297bf4d1d4f11c6b2a491e
Aleksandra Skierbiszewska <ola.skierbiszewska@norbsoft.com>
2015-10-24 22:09:13 +0200

    test commit

--- src/main/java/com/ola/Main.java
+++ src/main/java/com/ola/Main.java
@@ -6,6 +6,6 @@ package com.ola;
 public class Main {
 
     public static void main (String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world ");
     }
 }
